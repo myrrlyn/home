859866512ec096a684df5a74ae19174cd44b127c
myrrlyn <self@myrrlyn.net>
2023-10-28 01:43:07 -0500

    Update dart-sass

--- config/config.exs
+++ config/config.exs
@@ -36,7 +36,7 @@ config :phoenix, :json_library, Jason
 
 # Use the Dart SASS driver for CSS compilation.
 config :dart_sass,
-  version: "1.57.0",
+  version: "1.58.0",
   default: [
     args: ~w(-Inode_modules css:../priv/static/css),
     cd: Path.expand("../assets", __DIR__)
