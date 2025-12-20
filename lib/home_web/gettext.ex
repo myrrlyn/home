d5c5d5290a8b8d8a78d609a223b7b49749a23408
myrrlyn <self@myrrlyn.net>
2025-12-20 08:52:23 -0600

    updates

--- lib/home_web/gettext.ex
+++ lib/home_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule HomeWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :home
+  use Gettext.Backend, otp_app: :home
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Home.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      listeners: [Phoenix.CodeReloader]
     ]
   end
 
@@ -44,31 +45,32 @@ defmodule Home.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0"},
+      {:phoenix, "~> 1.8"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.14"},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:bandit, "~> 0.6.9"},
-      {:esbuild, "~> 0.6", runtime: Mix.env() == :dev},
-      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.22"},
+      {:phoenix_live_view, "~> 1.1"},
+      {:phoenix_live_dashboard, "~> 0.8"},
+      {:bandit, "~> 1.0"},
+      {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
+      {:dart_sass, "~> 0.7", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 1.1"},
+      {:telemetry_poller, "~> 1.3"},
+      {:gettext, "~> 0.20"},
       {:jason, "~> 1.4"},
       {:earmark, "~> 1.4"},
       {:earmark_parser, "~> 1.4"},
       {:ok, "~> 2.0"},
-      {:timex, "~> 3.7"},
-      {:yaml_elixir, "~> 2.9"},
+      {:timex, "~> 3.7.11"},
+      {:yaml_elixir, "~> 2.12"},
       {:yaml_front_matter, "~> 1.0"},
       # {:wyz, path: "../../Elixir/wyz"},
       {:wyz, git: "https://github.com/myrrlyn/wyz-ex.git"},
       {:exgravatar, "~> 2.0"},
       {:statistics, "~> 0.6"},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.28", only: [:dev], runtime: false},
-      {:toml, "~> 0.7"}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.38", only: [:dev], runtime: false},
+      {:toml, "~> 0.7"},
+      {:git_cli, "~> 0.3"}
     ]
   end
 
@@ -85,7 +87,7 @@ defmodule Home.MixProject do
       test: ["test"],
       "assets.deploy": [
         "cmd npm install --prefix assets",
-        "cmd cp -R assets/static/ priv/static/",
+        # "cmd cp -R assets/static/ priv/static/",
         "esbuild default --minify",
         "sass default --no-source-map --style=compressed",
         "phx.digest"
